204225d56d78d57d5d579814d475e9cff08b5f21
Apollo <88677238+ApolloDraco@users.noreply.github.com>
2021-08-11 14:22:02 -0400

    Create Welcome2.java

---
+++ Welcome2.java
@@ -0,0 +1,12 @@
+/* 
+ *  Description: This application displays 'Welcome to Java!' in a message dialog box. Part 2
+*/
+
+import java.x.swing.JOptionPane;
+public class WelcomeInMessageDialogBox {
+   public static void main (String [] args) {
+      JOptionPane.showMessageDialog(null, "Welcome to Java!");
+         }
+       }
+   }
+}
