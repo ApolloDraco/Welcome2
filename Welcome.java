518df4060d770f6f1afd4d2101d250951df0aa9d
Apollo <88677238+ApolloDraco@users.noreply.github.com>
2023-03-07 16:45:34 -0500

    Update and rename Welcome2.java to Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -1,9 +1,9 @@
 /* 
- *  Description: This application displays 'Welcome to Java!' in a message dialog box. Part 2
+ *  Description: This application displays 'Welcome to Java!' in a message dialog box.
 */
 
 import java.x.swing.JOptionPane;
-public class WelcomeInMessageDialogBox {
+public class Welcome {
    public static void main (String [] args) {
       JOptionPane.showMessageDialog(null, "Welcome to Java!");
          }
