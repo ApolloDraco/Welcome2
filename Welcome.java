1eb15bf70ddc6d15742317f6bfad5672d7a4087c
Apollo <88677238+ApolloDraco@users.noreply.github.com>
2023-04-11 17:00:31 -0400

    Update Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -2,11 +2,10 @@
  *  Description: This application displays 'Welcome to Java!' in a message dialog box.
 */
 
-import java.x.swing.JOptionPane;
+import javax.swing.JOptionPane;
+
 public class Welcome {
-   public static void main (String [] args) {
+   public static void main(String[] args) {
       JOptionPane.showMessageDialog(null, "Welcome to Java!");
-         }
-       }
    }
 }
